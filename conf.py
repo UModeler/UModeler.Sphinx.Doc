735c89342623cc87dd2380bf72894b5448d95782
tripolygon <jaesik.hwang@tripolygon.com>
2017-01-19 15:45:53 +0900

    Youtube extension has been removed.

--- conf.py
+++ conf.py
@@ -34,8 +34,7 @@ extensions = [
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-	'sphinxcontrib.youtube'
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
