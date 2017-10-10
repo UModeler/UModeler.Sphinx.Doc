f7b8f1eb09434d65e87ff2c93bf4d0a5a5a587c7
umodeler <umodelerforunity@tripolygon.com>
2017-10-10 22:55:20 +0900

    added fav icon

--- conf.py
+++ conf.py
@@ -158,7 +158,7 @@ html_logo = "images/UModeler_Logo_small.png"
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #
-# html_favicon = None
+html_favicon = "images/logo_fav_transparent_background.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
