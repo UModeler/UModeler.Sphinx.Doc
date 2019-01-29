d48b21bef5100ce85fff6dd91946a23ffc60a4e7
tripolygon <jaesik.hwang@tripolygon.com>
2019-01-29 18:03:33 +0900

    Renewal of Tweak, Surface and Misc Group

--- conf.py
+++ conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Unity UModeler Manual'
-copyright = '2017, Tripolygon'
+copyright = '2019, Tripolygon'
 author = 'Tripolygon'
 
 # The version info for the project you're documenting, acts as replacement for
